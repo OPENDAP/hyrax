37106d0fcadd3fed7691dca2b15e84b73aceca53
James Gallagher <jgallagher@opendap.org>
2017-12-03 10:54:31 -0700

    Moved the release notes to their own directory.

--- spath.sh
+++ spath.sh
@@ -28,9 +28,9 @@ export LD_LIBRARY_PATH=$prefix/lib:$prefix/deps/lib
 #
 # Added it back. The new tomcat 7 scripts don't require that 
 # CATALINA_HOME is set, so this is really for TC 6 compat. jhrg 12/30/14
-tc=`ls -d -1 $prefix/apache-tomcat-* 2> /dev/null | grep -v '.*\.tar\.gz'`
-if test -n "$tc"
-then
-    export TOMCAT_DIR=$tc
-    export CATALINA_HOME=$TOMCAT_DIR
-fi
+# tc=`ls -d -1 $prefix/apache-tomcat-* 2> /dev/null | grep -v '.*\.tar\.gz'`
+# if test -n "$tc"
+# then
+#     export TOMCAT_DIR=$tc
+#     export CATALINA_HOME=$TOMCAT_DIR
+# fi
