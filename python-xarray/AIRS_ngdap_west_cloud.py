0fcccc459e569fb584c996cf5de966c8bf946af1
James Gallagher <jgallagher@opendap.org>
2020-07-14 09:49:36 -0600

    Added more xarray access tests for our dmrpp code

--- python-xarray/AIRS_ngdap_west_cloud.py
+++ python-xarray/AIRS_ngdap_west_cloud.py
@@ -18,13 +18,21 @@ od_files = []
 for g in airs_granules.airs_366:
     od_files.append(base_url + g)
 
-print(od_files[0], '\n', od_files[-1])
+print('URL start and end:\n', od_files[0], '\n', od_files[-1])
 
-cloud_data = xa.open_mfdataset(od_files[0:2], engine='pydap', parallel=True, combine='by_coords')
+cloud_data = xa.open_mfdataset(od_files[0:2], data_vars='CloudTopPres_A', engine='pydap', parallel=True, combine='by_coords')
 
-cloud_ws = cloud_data['CloudTopPres_A'].sel(latitude=slice(-53.99, -14), longitude=slice(140, 170))
+print('Completed the xarray open_mfdataset\n')
 
-cloud_ws_mean = cloud_ws.mean(dim=['latitude', 'longitude'])
-cloud_ws_mean.plot.line()
+cloud_ws = cloud_data['CloudTopPres_A'].sel(Latitude=slice(-53.99, -14), Longitude=slice(140, 170))
+
+print('Completed the CloudTopPres_A selection\n')
+
+cloud_ws_mean = cloud_ws.mean(dim=['Latitude', 'Longitude'])
+
+print('Computed the mean of the CloudTopPres_A values\n')
+print(cloud_ws_mean)
+
+# cloud_ws_mean.plot.line()
 
 
