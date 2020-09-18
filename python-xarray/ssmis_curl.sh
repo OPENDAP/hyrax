0e75588746e82d9e0b45667e2f9bf7e83e97ddd7
ndp-opendap <ndp@opendap.org>
2020-09-18 16:50:20 -0700

    Adding new targets to curl test

--- python-xarray/ssmis_curl.sh
+++ python-xarray/ssmis_curl.sh
@@ -7,14 +7,35 @@ data_file="ssmis_result.csv"
 
 
 function run_curl(){
-    time -p curl -s -L -c cookie -b cookie ${dap_url}${dap_suffix}?${constraint} >> ${data_file}
+    time -p curl -s -n -L -c cookie -b cookie ${dap_url}${dap_suffix}?${constraint} >> ${data_file}
 }
+
+function use_tea_uat {
+    server_url="http://ngap-west.opendap.org/opendap/ssmis/tea-uat"
+    granule_suffix=".dmrpp"
+    echo "Using TEA in UAT"
+}
+
+function use_ngap_west {
+    server_url = 'http://ngap-west.opendap.org/opendap/ngap/providers/GHRC_CLOUD/collections/' \
+                        'RSS%20SSMIS%20OCEAN%20PRODUCT%20GRIDS%20DAILY%20FROM%20DMSP%20F16%20NETCDF%20V7/granules/'
+    granule_suffix = ""
+    echo "Using NGAP Service (us-west-2)"
+}
+
+function use_ngap_uat {
+    server_url = 'http://opendap.uat.earthdata.nasa.gov/providers/GHRC_CLOUD/collections/' \
+                        'RSS%20SSMIS%20OCEAN%20PRODUCT%20GRIDS%20DAILY%20FROM%20DMSP%20F16%20NETCDF%20V7/granules/'
+    granule_suffix = ""
+    echo "Using NGAP Service (us-west-2)"
+}
+
 
 
 function run_ssmis() {
     mark=${1};
-    server_url="http://ngap-west.opendap.org/opendap/ssmis/tea-uat"
-    granule_suffix=".dmrpp"
+
+    use_ngap_uat
 
     echo "# -- --  -- -- SSMIS wind_speed subset BEGIN"
     granules=`cat ${granules_file} | awk '{if(NR<91){n=split($0,s,"\"");if(NF==2){print s[3];}else{print s[2];}}}'`
@@ -40,10 +61,10 @@ function curl_run1000() {
     rm -f ${data_file} ${data_file}.time
 
     for i in {1..125}; do
-        echo "----- LAP: $i Started: "`date`"  ut: "`date "+%s"`
-        for process in {1..8}; do
+        echo "----- LAP: $i Started: "`date`"  uTime: "`date "+%s"`
+        #for process in {1..8}; do
             run_ssmis "${i}-${process}" & 2>&1
-        done
+        #done
         wait;
     done
 }
