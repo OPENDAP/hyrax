9b6f976531264fb8b6bd47a9cb67e2878f68e114
ndp-opendap <ndp@opendap.org>
2020-09-19 03:49:09 -0700

    Adding new targets to curl test

--- python-xarray/ssmis_curl.sh
+++ python-xarray/ssmis_curl.sh
@@ -58,7 +58,7 @@ function curl_run1000() {
 
     rm -f ${data_file} ${data_file}.time
 
-    for i in {1..125}; do
+    for i in {1..500}; do
         echo "----- LAP: $i Started: "`date`"  uTime: "`date "+%s"`
         for process in {1..8}; do
             run_ssmis "${i}-${process}" & 2>&1
